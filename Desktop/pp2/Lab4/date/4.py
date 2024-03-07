862d6461b2fb1b3bc0257992fc515a0a6c024321
ZhubatkhanAidar <161718826+ZhubatkhanAidar@users.noreply.github.com>
2024-03-07 14:29:31 +0500

    4.py

--- Desktop/pp2/Lab4/date/4.py
+++ Desktop/pp2/Lab4/date/4.py
@@ -1,3 +1,11 @@
-print(bool("abc"))
+from datetime import datetime,timedelta
 
-True
+date1str=input()
+date2str=input()
+
+date1t=datetime.strptime(date1str,"%Y-%m-%d")
+date2t=datetime.strptime(date2str,"%Y-%m-%d")
+
+dif=(date1t-date2t).total_seconds()
+
+print(dif)
