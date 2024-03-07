8433558815fde745fd21c515e9bc7f4fb86a5ad2
ZhubatkhanAidar <161718826+ZhubatkhanAidar@users.noreply.github.com>
2024-03-07 14:28:11 +0500

    1.py

--- Desktop/pp2/Lab4/date/1.py
+++ Desktop/pp2/Lab4/date/1.py
@@ -1,3 +1,12 @@
-print(10 > 9)
+import datetime
+today = datetime.datetime.now()
+sub5 = today - datetime.timedelta(days=5)
+print(sub5)
 
-True
+
+'''
+import datetime
+today=datetime.datetime.now()
+five=today + datetime.timedelta(5)
+print(five.strftime("%A"))
+'''
