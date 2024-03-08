bf333376ba42291bf5c9c6f9fcce3543604d8a4d
ZhubatkhanAidar <161718826+ZhubatkhanAidar@users.noreply.github.com>
2024-03-08 15:50:35 +0500

    Update 5.py

--- Desktop/pp2/Lab5/regex/5.py
+++ Desktop/pp2/Lab5/regex/5.py
@@ -2,7 +2,7 @@
 
 import re
 
-with open(r'C:\Users\ASUS\Desktop\pp2\lab5\a.txt', 'r', encoding='utf-8') as file:
+with open(r'C:\Users\Aidar\Desktop\pp2\lab5\a.txt', 'r', encoding='utf-8') as file:
     g = file.read()
 
 match = re.findall(r"а.*б$", g)
