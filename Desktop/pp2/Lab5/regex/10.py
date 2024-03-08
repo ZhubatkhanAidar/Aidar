0733cddedc101b28a3d6b0e11caefcfc526c203e
ZhubatkhanAidar <161718826+ZhubatkhanAidar@users.noreply.github.com>
2024-03-08 15:49:13 +0500

    10.py

--- Desktop/pp2/Lab5/regex/10.py
+++ Desktop/pp2/Lab5/regex/10.py
@@ -5,9 +5,8 @@ def fun(c):
     s = re.sub(r'(?<=[a-zа-я0-9])([A-ZА-Я])', r'_\1', c)
     return s.lower()
 
-with open(r'C:\Users\ASUS\Desktop\pp2\lab5\a.txt', 'r', encoding='utf-8') as file:
+with open(r'C:\Users\Aidar\Desktop\pp2\lab5\a.txt', 'r', encoding='utf-8') as file:
      g = file.read()
 
 result = fun(g)
 print(result)
-
