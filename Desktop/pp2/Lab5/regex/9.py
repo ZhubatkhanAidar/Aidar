fea5461de636fa18c73567f9412794b58e057b76
ZhubatkhanAidar <161718826+ZhubatkhanAidar@users.noreply.github.com>
2024-03-08 15:51:37 +0500

    Update 9.py

--- Desktop/pp2/Lab5/regex/9.py
+++ Desktop/pp2/Lab5/regex/9.py
@@ -2,7 +2,7 @@
 
 import re
 
-with open(r'C:\Users\ASUS\Desktop\pp2\lab5\a.txt', 'r', encoding='utf-8') as file:
+with open(r'C:\Users\Aidar\Desktop\pp2\lab5\a.txt', 'r', encoding='utf-8') as file:
      g = file.read()
 
 insertie = re.sub(r'[А-Я]',' \1',g)
