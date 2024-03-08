30546ea5c65d6b3a4a841939d06f07575bf279d0
ZhubatkhanAidar <161718826+ZhubatkhanAidar@users.noreply.github.com>
2024-03-08 15:51:09 +0500

    Update 7.py

--- Desktop/pp2/Lab5/regex/7.py
+++ Desktop/pp2/Lab5/regex/7.py
@@ -7,7 +7,7 @@ def fun(s):
     c = word[0] + ''.join(word.capitalize() for word in word[1:])
     return c
 
-with open(r'C:\Users\ASUS\Desktop\pp2\lab5\a.txt', 'r', encoding='utf-8') as file:
+with open(r'C:\Users\Aidar\Desktop\pp2\lab5\a.txt', 'r', encoding='utf-8') as file:
      g = file.read()
 
 result = fun(g)
